95ad0e7632d01b91d813cb6e2c628d27bb6c9786
shashibabu123 <goddream480@gmail.com>
2025-04-16 10:02:50 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "strapi" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update -y
+              sudo apt install -y curl git
+              curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+              sudo apt install -y nodejs
+              sudo npm install -g yarn pm2
+
+              git clone https://github.com/<your-username>/<your-strapi-repo>.git
+              cd <your-strapi-repo>
+              yarn install
+              yarn build
+              pm2 start yarn --name strapi -- develop
+              EOF
+
+  tags = {
+    Name = "StrapiServer"
+  }
+}
